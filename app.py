21688948af3a7ce549eae75466e8a223f87abaaf
codeWithGit123 <mdabdur2004@gmail.com>
2024-11-03 19:54:10 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Hello World")
+
+st.header("Welcome To my First Streamlit app")
+st.subheader("This is an Sub-Header")
+
+st.text("Streamlit is an Awesome Tool, Simple text")
