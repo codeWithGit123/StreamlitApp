c77b160f9d3a2a1a88122e1be9e1bef99c55aac7
codeWithGit123 <mdabdur2004@gmail.com>
2024-11-04 00:46:15 +0530

    Hello

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
-st.title("Hello World")
+from sklearn.datasets import load_diabetes,load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import os
 
-st.header("Welcome To my First Streamlit app")
-st.subheader("This is an Sub-Header")
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-st.text("Streamlit is an Awesome Tool, Simple text")
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20,random_state=42)
+
+model = LogisticRegression(max_iter=2000,solver='lbfgs')
+
+model.fit(X_train,y_train)
+
+yp = model.predict(X_test)
+
+st.title("Iris Plant Predictor")
+st.subheader("Select the Values")
+
+a = st.number_input("Enter Sepal Length")
+b = st.number_input("Enter Sepal Width")
+c = st.number_input("Enter Petal Length")
+d = st.number_input("Enter Petat Width")
+
+
+if st.button("Predict"):
+    inp = [[a,b,c,d]]
+    pred = iris.target_names[model.predict(inp)]
+    st.write(f"The Plant is: {pred[0]}")
+    st.image(os.path.join(os.getcwd(),"static",f"{pred[0]}.jpeg"))
+    st.write(f"The Accuracy Score is:{accuracy_score(yp,y_test)}")
